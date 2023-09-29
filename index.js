7335ec118f10597e1b303bc71513dc6056ff3071
samuel <saespinosab@unal.edu.co>
2023-09-28 19:22:19 -0500

    producto minimo viable

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+import { Translator } from "./Model/Translator.js";
+
+fetch('http://localhost:8080/assets/diccionario.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La red esta malaaa');
+    }
+    return response.json(); 
+  })
+  .then(data => {
+    localStorage.setItem("diccionario",JSON.stringify(data[0]));
+  })
+  .catch(error => {
+    console.error('murio el fetch', error);
+});
+
 let input = document.querySelector('#to_translate');
 let btn=document.querySelector('#send_btn');
+let user_translator=new Translator(JSON.parse(localStorage.getItem("diccionario")),{});
 input.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     e.preventDefault();
-  } else {console.log(input)}
+    user_translator.translate(input.value);
+    pageScroll();
+    input.value="";
+  } 
 });
 btn.addEventListener("click",(e)=>{
     e.preventDefault();
+    user_translator.translate(input.value);
+    pageScroll();
+    input.value=""; 
 });
+function pageScroll() {
+    document.querySelector("main").scrollTo({ top: document.querySelector("main").scrollHeight, behavior: 'smooth' });
+}
